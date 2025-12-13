3a353b5b451cec50e79ffd3ca5d2e34841b007eb
bilal <biluthescorpional@gmail.com>
2025-12-13 20:55:43 +0500

    syncing changes and bugfixing

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
-#include <iostream>
+//
+// Created by Muhammad Bilal on 12/9/2025.
+//
+
+#include<iostream>
 #include "external/sqlite/sqlite3.h"
 
 int main() {
-	
     sqlite3* db;
-    int rc = sqlite3_open("test.db", &db);
+    int rc = sqlite3_open("library.db", &db);
 
-    if(rc) {
-        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
-    } else {
-        std::cout << "Opened database successfully!" << std::endl;
+    if (rc)
+    {
+        std::cerr<<"Can't open database: "<<sqlite3_errmsg(db)<<std::endl;
+        return 1;
     }
+    else
+        std::cout<<"Database Opened Successfully!"<<std::endl;
 
     sqlite3_close(db);
+
+    std::cout<<"Press Enter to Exit...";
+    std::cin.get();
     return 0;
 }
